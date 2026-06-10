014e26ff0c28f53d6365ffc0c770bbcd8916140e
Rx_kdj <djkdj77222@gmail.com>
2026-06-10 17:55:40 +0530

    first commit

---
+++ src/app/routes.ts
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+import Portfolio from "./pages/Portfolio";
+import ProjectAdmin from "./pages/ProjectAdmin";
+
+export const router = createBrowserRouter([
+  { path: "/", Component: Portfolio },
+  { path: "/kdj", Component: ProjectAdmin },
+]);
